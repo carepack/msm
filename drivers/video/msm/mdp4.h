f7f1517943e6c5f5ff43e9928e0cf69141745c90
carepack <meit130183@gmail.com>
2015-07-24 07:47:33 +0000

    global function is unused. removed static for gcc

--- drivers/video/msm/mdp4.h
+++ drivers/video/msm/mdp4.h
@@ -861,7 +861,7 @@ static inline void mdp4_overlay_dsi_video_start(void)
 	/* empty */
 }
 
-static int mdp4_dsi_video_splash_done(void)
+int mdp4_dsi_video_splash_done(void)
 {
 }
 #endif /* CONFIG_FB_MSM_MIPI_DSI */
